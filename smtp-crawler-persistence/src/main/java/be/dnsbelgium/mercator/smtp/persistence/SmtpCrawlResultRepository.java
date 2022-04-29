7884d788808f36c97b7082e4f663b23d14871cf6
Aroen van Remmen <aroen.van.remmen@dnsbelgium.be>
2022-04-29 15:51:40 +0200

    Adjusted smtp-repo to contain @param

--- smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/SmtpCrawlResultRepository.java
+++ smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/SmtpCrawlResultRepository.java
@@ -1,12 +1,13 @@
 package be.dnsbelgium.mercator.smtp.persistence;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface SmtpCrawlResultRepository extends PagingAndSortingRepository<SmtpCrawlResult, Long> {
 
-    Optional<SmtpCrawlResult> findFirstByVisitId(UUID visitId); // TODO: Fix duplicate visitIds.
+    Optional<SmtpCrawlResult> findFirstByVisitId(@Param("visitId") UUID visitId); // TODO: Fix duplicate visitIds.
 
 }
