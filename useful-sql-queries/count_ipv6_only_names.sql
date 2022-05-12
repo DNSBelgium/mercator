fea44e15b299f47764b367f5ac85378b23dcfdbe
Quentin Loos <quentin.loos@dnsbelgium.be>
2022-05-12 15:16:32 +0200

    PA-13877: Review queries

--- useful-sql-queries/count_ipv6_only_names.sql
+++ useful-sql-queries/count_ipv6_only_names.sql
@@ -1,9 +1,12 @@
 -- Count amount of prefixes with AAAA records but no A records (per label)
 
-SELECT coalesce(l.labels, 'no_label') AS label, domain_name
-FROM dns_crawler.request req
-    LEFT OUTER JOIN dispatcher.dispatcher_event_labels l ON l.visit_id = req.visit_id
-    JOIN dns_crawler.response resp ON resp.request_id = req.id
-GROUP BY domain_name, coalesce(l.labels, 'no_label')
-HAVING SUM(CASE WHEN record_type = 'A' THEN 1 ELSE 0 END) = 0
-   AND SUM(CASE WHEN record_type = 'AAAA' THEN 1 ELSE 0 END) > 0
+SELECT label, domain_name
+FROM (
+         SELECT coalesce(l.labels, 'no_label') AS label, domain_name, SUM(CASE WHEN record_type = 'A' THEN 1 ELSE 0 END) number_a_record, SUM(CASE WHEN record_type = 'AAAA' THEN 1 ELSE 0 END) number_aaaa_record
+         FROM dns_crawler.request req
+                  LEFT OUTER JOIN dispatcher.dispatcher_event_labels l ON l.visit_id = req.visit_id
+                  JOIN dns_crawler.response resp ON resp.request_id = req.id
+         GROUP BY label, domain_name
+     ) sub
+WHERE number_a_record = 0
+  AND number_aaaa_record > 0
