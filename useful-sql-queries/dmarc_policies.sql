13bf303c918bb0bab527d1f7c8502d08899e37af
Quentin Loos <quentin.loos@dnsbelgium.be>
2022-05-12 13:50:57 +0200

    PA-13877: Review queries

--- useful-sql-queries/dmarc_policies.sql
+++ useful-sql-queries/dmarc_policies.sql
@@ -1,23 +1,18 @@
 -- Get count of NOT NULL _dmarc prefixes to see how have a record_data containing 'none', 'quarantine' or 'reject'.
 
-WITH groups AS (
-    SELECT CASE
-        WHEN (req.prefix = '_dmarc' AND res.record_data ~ 'v=DMARC1') THEN
-            CASE
-                WHEN (res.record_data ~ 'p=none') THEN 'none'
-                WHEN (res.record_data ~ 'p=quarantine') THEN 'quarantine'
-                WHEN (res.record_data ~ 'p=reject') THEN 'reject'
-                ELSE 'no_policy'
-                END
-            ELSE 'not_dmarc'
-            END p,
-        l.labels
-    FROM dns_crawler.request req JOIN dns_crawler.response res
-        ON req.id = res.request_id
-        JOIN dispatcher.dispatcher_event_labels l
-            ON req.visit_id = l.visit_id
-    WHERE req.record_type = 'TXT' AND req.visit_id = l.visit_id
-)
-SELECT p, COUNT(*) amount
-FROM groups
-GROUP BY p;
+SELECT req.domain_name,
+       CASE
+           WHEN (req.prefix = '_dmarc' AND resp.record_data ~ 'v=DMARC1') THEN
+               CASE
+                   WHEN (resp.record_data ~ 'p=none') THEN 'none'
+                   WHEN (resp.record_data ~ 'p=quarantine') THEN 'quarantine'
+                   WHEN (resp.record_data ~ 'p=reject') THEN 'reject'
+                   ELSE 'no_policy'
+                   END
+           ELSE 'not_dmarc'
+           END p
+FROM dns_crawler.request req
+         JOIN dns_crawler.response resp ON resp.request_id = req.id
+WHERE req.record_type = 'TXT'
+  AND req.prefix = '_dmarc'
+  AND resp.record_data ~ 'v=DMARC1'
