a3fa52b06818670289293feda2183a071870b1a4
Quentin Loos <quentin.loos@dnsbelgium.be>
2022-05-25 08:22:30 +0200

    PA-15204: FooTest

--- dns-crawler/src/test/java/be/dnsbelgium/mercator/dns/FooTest.java
+++ dns-crawler/src/test/java/be/dnsbelgium/mercator/dns/FooTest.java
@@ -1,21 +1,38 @@
 package be.dnsbelgium.mercator.dns;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.xbill.DNS.*;
 import org.xbill.DNS.Record;
+import org.xbill.DNS.*;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.List;
 
-@ActiveProfiles({"local", "test"})
-@SpringBootTest
 public class FooTest {
+
+    private Lookup initLookup(Name name, int value) throws UnknownHostException {
+        Lookup lookup = new Lookup(name, value);
+        lookup.setResolver(newSimpleResolver());
+        return lookup;
+    }
+
+    @Test
+    void bar() throws IOException {
+        final Name full = Name.concatenate(Name.fromString("dnsbelgium.be"), Name.root);
+        Lookup lookup = initLookup(full, Type.value("RRSIG"));
+        lookup.run();
+        Record[] answers = lookup.getAnswers();
+        for (Record answer : answers) {
+            System.out.println(" --- ");
+            System.out.println(answer.toString());
+            System.out.println(" --- ");
+        }
+    }
+
     @Test
     void foo() throws IOException {
         final Name full = Name.concatenate(Name.fromString("dnsbelgium.be"), Name.root);
-        final Resolver res = newResolver();
+        final Resolver res = newSimpleResolver();
         final Record question = Record.newRecord(full, Type.SOA, DClass.IN);
         final Message query = Message.newQuery(question);
         final Message response = res.send(query);
@@ -29,8 +46,15 @@ public class FooTest {
             }
         }
     }
-    private static Resolver newResolver() {
-        final Resolver res = new ExtendedResolver();
+    private static Resolver newExtendedResolver() throws UnknownHostException {
+        final Resolver res = new ExtendedResolver(new String[]{"8.8.8.8"});
+        res.setEDNS(0, 0, ExtendedFlags.DO, (List<EDNSOption>) null);
+        res.setIgnoreTruncation(false);
+        return res;
+    }
+
+    private static Resolver newSimpleResolver() throws UnknownHostException {
+        final Resolver res = new SimpleResolver("8.8.8.8");
         res.setEDNS(0, 0, ExtendedFlags.DO, (List<EDNSOption>) null);
         res.setIgnoreTruncation(false);
         return res;
