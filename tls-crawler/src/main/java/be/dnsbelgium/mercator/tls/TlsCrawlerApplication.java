f00f7cfc9d5b63a0db75fa2cd55b52ee9674ffc7
Maarten Bosteels <maarten.bosteels@dnsbelgium.be>
2022-06-05 10:54:10 +0200

    PA-14979 : empty module for tls-crawler

---
+++ tls-crawler/src/main/java/be/dnsbelgium/mercator/tls/TlsCrawlerApplication.java
@@ -0,0 +1,13 @@
+package be.dnsbelgium.mercator.tls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlsCrawlerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TlsCrawlerApplication.class, args);
+  }
+
+}
