7faaf9bd40fd8c86d135b49d9a03b3767d51903d
Quentin Loos <quentin.loos@dnsbelgium.be>
2022-07-19 21:38:19 +0200

    EB-000: Remove UNDEFINED default env variables. Let the code handle default values

--- muppets/src/config.ts
+++ muppets/src/config.ts
@@ -7,29 +7,13 @@ interface MuppetsConfiguration {
     server_port: number
 };
 
-function parse_env_string_or_default<T>(name: string, default_value: T): string | T {
-    const value = process.env[name];
-    if ((value === undefined) || (value.toUpperCase() === "UNDEFINED"))
-        return default_value;
-    else
-        return value;
-}
-
-function parse_env_number_or_default<T>(name: string, default_value: T): number | T {
-    const value = process.env[name];
-    if ((value === undefined) || (value.toUpperCase() === "UNDEFINED"))
-        return default_value;
-    else
-        return parseInt(value);
-}
-
 const env_config: MuppetsConfiguration  = {
-    s3_endpoint: parse_env_string_or_default("S3_ENDPOINT", null),
-    s3_bucket_name: parse_env_string_or_default("S3_BUCKET_NAME", "mercator-muppets"),
-    sqs_endpoint: parse_env_string_or_default("SQS_ENDPOINT", null),
-    sqs_input_queue: parse_env_string_or_default("SQS_INPUT_QUEUE", "mercator-muppets-input"),
-    sqs_output_queue: parse_env_string_or_default("SQS_OUTPUT_QUEUE", "mercator-muppets-output"),
-    server_port: parse_env_number_or_default("SERVER_PORT", 8085),
+    s3_endpoint: process.env.S3_ENDPOINT || null,
+    s3_bucket_name: process.env.S3_BUCKET_NAME || "mercator-muppets",
+    sqs_endpoint: process.env.SQS_ENDPOINT || null,
+    sqs_input_queue: process.env.SQS_INPUT_QUEUE || "mercator-muppets-input",
+    sqs_output_queue: process.env.SQS_OUTPUT_QUEUE || "mercator-muppets-output",
+    server_port: parseInt(process.env.SERVER_PORT || "8085"),
 };
 
 console.log(`Using configuration:`);
