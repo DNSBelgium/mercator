78b89c4e05eb944c2a01285a7d8f87809c18106e
Quentin Loos <quentin.loos@dnsbelgium.be>
2022-07-27 09:22:27 +0200

    PA-14979: more versions from Spring boot dependencies

--- tls-crawler/src/main/java/be/dnsbelgium/mercator/tls/TlsCrawlerApplication.java
+++ tls-crawler/src/main/java/be/dnsbelgium/mercator/tls/TlsCrawlerApplication.java
@@ -1,16 +1,20 @@
 package be.dnsbelgium.mercator.tls;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.security.Security;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @SpringBootApplication
 public class TlsCrawlerApplication {
 
+  private static final Logger logger = getLogger(TlsCrawlerApplication.class);
+
   public static void main(String[] args) {
-    System.out.println("Starting TlsCrawlerApplication");
-    System.out.println("setting security property \"jdk.tls.disabledAlgorithms\" to \"NULL\"");
+    logger.info("setting security property \"jdk.tls.disabledAlgorithms\" to \"NULL\"");
     Security.setProperty("jdk.tls.disabledAlgorithms", "NULL");
     SpringApplication.run(TlsCrawlerApplication.class, args);
   }
