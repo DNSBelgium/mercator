2d4c267613f6fb34ce6ca8f0d51117e4bc4818ff
Maarten Bosteels <maarten.bosteels@dnsbelgium.be>
2022-08-01 20:21:13 +0200

    PA-000 : attempt to fix aws library version conflict in feature-extractor

--- feature-extraction/src/test/java/be/dnsbelgium/mercator/feature/extraction/FeatureCrawlerTest.java
+++ feature-extraction/src/test/java/be/dnsbelgium/mercator/feature/extraction/FeatureCrawlerTest.java
@@ -4,6 +4,8 @@ import be.dnsbelgium.mercator.content.persistence.ContentCrawlResultRepository;
 import be.dnsbelgium.mercator.feature.extraction.persistence.HtmlFeaturesRepository;
 import be.dnsbelgium.mercator.test.LocalstackContainer;
 import be.dnsbelgium.mercator.test.PostgreSqlContainer;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +21,7 @@ import org.testcontainers.junit.jupiter.Container;
 
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @ActiveProfiles({"local", "test"})
@@ -35,6 +38,8 @@ class FeatureCrawlerTest {
 
   @Container static LocalstackContainer localstack = new LocalstackContainer();
   @Container static PostgreSqlContainer pgsql = PostgreSqlContainer.getInstance();
+
+  @Autowired AmazonSQSClientBuilder sqsClientBuilder;
 
   private static final Logger logger = getLogger(FeatureCrawlerTest.class);
 
@@ -63,6 +68,16 @@ class FeatureCrawlerTest {
 
   @Test
   public void loadApplicationContext() {
+  }
+
+  @Test
+  public void getQueueUrl() {
+    String queueName = environment.getProperty("feature.extraction.input.queue.name");
+    AmazonSQS sqs = sqsClientBuilder.build();
+    // this fails when we have conflicting library versions on the classpath
+    String queueUrl = sqs.getQueueUrl(queueName).getQueueUrl();
+    logger.info("queueUrl = {}", queueUrl);
+    assertThat(queueUrl).isNotNull();
   }
 
 }
