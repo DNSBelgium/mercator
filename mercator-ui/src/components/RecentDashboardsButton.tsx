df7f7d355f92b43085460b6f5b3b06e6f9d19cd5
Ward Segers <ward.segers@dnsbelgium.be>
2022-09-14 15:00:44 +0200

    PA-15167: don't show date on button and disable dropdown

--- mercator-ui/src/components/RecentDashboardsButton.tsx
+++ mercator-ui/src/components/RecentDashboardsButton.tsx
@@ -12,7 +12,7 @@ function RecentDashboardsButton() {
     async function dashboardExists(month: number, year: number): Promise<DashboardEntry | null> {
         const label = `${year.toString().padStart(4, '0')}-${month.toString().padStart(2, '0')}`;
         const url = (window as any)._env_.REACT_APP_MUPPETS_HOST + `/dashboard/${label}/mercator_full_crawl_dashboard.html`;
-        const response = await fetch(url,{ method: "HEAD" } );
+        const response = await fetch(url,{ method: "HEAD" } ).catch(err => { return {"status": 404}; });
         return response.status === 200 ? { label, url } : null;
     }
 
@@ -57,7 +57,7 @@ function RecentDashboardsButton() {
         );
         else
             return (
-                <span>Dashboard ({ recentDashboards[0].label })</span>
+                <span>Dashboard</span>
             );
     }
 
@@ -66,7 +66,7 @@ function RecentDashboardsButton() {
             return <Dropdown.Menu></Dropdown.Menu>;
 
         const versionDropdown = recentDashboards.map((item, idx) => (
-            <Dropdown.Item href={ item.url } target="_blank">{ item.label }{ idx == 0 ? " (latest)" : "" }</Dropdown.Item>
+            <Dropdown.Item href={ item.url } target="_blank">{ item.label }{ idx === 0 ? " (latest)" : "" }</Dropdown.Item>
         ));
         return (
            <Dropdown.Menu>
@@ -79,10 +79,10 @@ function RecentDashboardsButton() {
     const recentDashboardsButton = () => {
         return (
             <Dropdown as={ButtonGroup}>
-                      <Button disabled={recentDashboards === null} href={recentDashboards !== null ? recentDashboards[0].url : "#"}>
-                          { buttonContents() }
-                      </Button>
-                      <Dropdown.Toggle split />
+                <Button disabled={recentDashboards === null} href={recentDashboards !== null ? recentDashboards[0].url : "#"}>
+                    { buttonContents() }
+                </Button>
+                <Dropdown.Toggle split disabled={recentDashboards === null}/>
                 { dropdownContents() }
             </Dropdown>
         );
