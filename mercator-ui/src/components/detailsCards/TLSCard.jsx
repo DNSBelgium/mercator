bbdac3a93f02cf48b2dee0718ff9afc66591961d
VanBenedenMaarten <r0751858@ucll.be>
2023-04-18 16:13:22 +0200

    Updated TlsCard after code review

--- mercator-ui/src/components/detailsCards/TLSCard.jsx
+++ mercator-ui/src/components/detailsCards/TLSCard.jsx
@@ -1,10 +1,8 @@
 import {useEffect, useState} from "react";
 import api from "../../services/api";
 import {Card, Col, Row, Table} from "react-bootstrap";
-import {checkObjectIsFalsy} from "../../services/Util";
+import {checkObjectIsFalsy, differenceBetweenTwoDates} from "../../services/Util";
 import moment from "moment";
-
-require('moment-precise-range-plugin');
 
 const TLSCard = (props) => {
 
@@ -19,7 +17,7 @@ const TLSCard = (props) => {
     useEffect(() => {
         const handlerData = async () => {
             let certificateResponse;
-            let crawlResultResponse;
+            let crawlResultResponse = [];
             let fullScanResponse;
             let fullScans = {};
             let certificateChain = {};
@@ -29,7 +27,9 @@ const TLSCard = (props) => {
             try {
                 crawlResultResponse = await api.get(`/crawlResultEntities/search/findByVisitId?visitId=${visitId}`);
                 crawlResultResponse = crawlResultResponse.data._embedded.crawlResultEntities;
-                if (crawlResultResponse.length === 0) throw "Error: 404 crawlResultResponse is empty";
+                if (crawlResultResponse.length === 0) {
+                    setErrorMessages(errorMessages => [...errorMessages, "No crawl data found for this visit."]);
+                }
             } catch (e) {
                 handleError(e, "crawl");
             }
@@ -44,8 +44,7 @@ const TLSCard = (props) => {
 
                 try {
                     let chain = [];
-                    certificateResponse = await api.get(`/certificateEntities/search/findByCrawlResultId?crawlResultId=${crawlResultResponse[i].id}`);
-                    // Convert array of objects to object with id as key: https://stackoverflow.com/a/44325124
+                    certificateResponse = await api.get(`/certificateEntities/search/findByCrawlResultId?crawlResultId=${crawlResultResponse[i].id}`)
                     certificateResponse = certificateResponse.data;
                     chain.push(certificateResponse);
                     let fingerprintForSearch = certificateResponse.signedBySha256;
@@ -82,35 +81,24 @@ const TLSCard = (props) => {
         return firstDate < secondDate;
     }
 
-    const differenceBetweenTwoDates = (date1, date2) => {
-        const firstDate = new moment(date1);
-        const secondDate = new moment(date2);
-        //https://github.com/codebox/moment-precise-range
-        const diff = moment.preciseDiff(firstDate, secondDate, true);
-        let string = "";
-        if (diff.years > 0) {
-            if (diff.years === 1) {
-                string += (diff.years + " year ");
-            } else {
-                string += (diff.years + " years ");
-            }
-        }
-        if (diff.months > 0) {
-            string += (diff.months + " months ");
-        }
-        if (diff.days > 0) {
-            string += (diff.days + " days ");
-        }
-        return string;
-    }
-
     const handleError = (e, position) => {
         let errorMessage;
-        if ((e.toString()).includes("404")) {
-            errorMessage = "No " + position + " data was found for this visit.";
-        } else if ((e.toString()).includes("500")) {
-            errorMessage = "Something went wrong while trying to contact the back-end.";
-        } else {
+        try {
+            switch (e.response.status) {
+                case 404:
+                    errorMessage = "No " + position + " data was found for this visit.";
+                    break;
+                case 500:
+                case 501:
+                case 502:
+                case 503:
+                    errorMessage = "Something went wrong while trying to contact the back-end.";
+                    break;
+                default:
+                    errorMessage = "Something unexpected went wrong.";
+            }
+        }
+        catch (e){
             errorMessage = "Something unexpected went wrong.";
         }
         setErrorMessages(errorMessages => [...errorMessages, errorMessage]);
@@ -123,25 +111,27 @@ const TLSCard = (props) => {
         }
         let array = csv.split(",");
         return <div>{array.map((x, i) => {
-            return <p key={i}>{x}</p>
+            return <p key={i} className="m-0 p-0">{x}</p>
         })}</div>;
     }
 
     const renderXCheck = (bool) => {
-        return bool ?
+        let className;
+        let drawing;
+        if (bool) {
+            className = "bi bi-x";
             //https://icons.getbootstrap.com/icons/x/
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                 className="bi bi-x" viewBox="0 0 16 16">
-                <path
-                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-            </svg>
-            :
+            drawing = "M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z";
+        }
+        else {
+            className = "bi bi-check";
             //https://icons.getbootstrap.com/icons/check/
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                 className="bi bi-check" viewBox="0 0 16 16">
-                <path
-                    d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
-            </svg>
+            drawing = "M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z";
+        }
+        return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                    className={className} viewBox="0 0 16 16">
+                    <path d={drawing}/>
+               </svg>
     }
 
     const renderCertificate = (type, certificate, crawlResult) => {
@@ -265,12 +255,6 @@ const TLSCard = (props) => {
             )
         }
         return crawlResult.map((result) => {
-            if (checkObjectIsFalsy(fullScan)) {
-                return null;
-            }
-            if (checkObjectIsFalsy(certificates)) {
-                return null;
-            }
             let scan = fullScan[result.id];
             let certs = certificates[result.id];
             return (
@@ -295,25 +279,29 @@ const TLSCard = (props) => {
                                                 <th>Host name matches certificate</th>
                                                 <td>{result.hostNameMatchesCertificate ? "Yes" : "No"}</td>
                                             </tr>
+                                            {scan !== undefined ? <>
                                             <tr>
-                                                <th>Full scan timestamp</th>
+                                                <th>Scan timestamp</th>
                                                 <td>{moment(scan.crawlTimestamp).format("DD/MM/YYYY HH:mm:ss")}</td>
                                             </tr>
                                             <tr>
-                                                <th>Full scan ip</th>
+                                                <th>Scanned ip</th>
                                                 <td>{scan.ip}</td>
                                             </tr>
                                             <tr>
-                                                <th>Full scan server name</th>
+                                                <th>Scan server name</th>
                                                 <td>{scan.serverName}</td>
                                             </tr>
                                             <tr>
-                                                <th>Full scan connect ok</th>
+                                                <th>Scan connect ok</th>
                                                 <td>{scan.connectOk ? "Yes" : "No"}</td>
                                             </tr>
+                                            </>
+                                            : ""}
                                             </tbody>
                                         </Table>
                                         <h4 className="mb-1 text-md-left">Protocols</h4>
+                                        {scan !== undefined ?
                                         <Table size="sm" borderless>
                                             <thead>
                                             <tr>
@@ -376,6 +364,7 @@ const TLSCard = (props) => {
                                             </tr>
                                             </tbody>
                                         </Table>
+                                            : "No scan data was found for this crawl result"}
                                     </div>
                                     <h4 className="mb-1 pt-3 text-md-left">Certificate chain</h4>
                                     {certs === undefined ? "No certificates were found for this crawl" :
