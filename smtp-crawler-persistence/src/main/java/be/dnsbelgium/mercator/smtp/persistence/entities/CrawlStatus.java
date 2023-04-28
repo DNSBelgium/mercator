f1c1e5f322867fab889ac707409901000dbfa263
VanBenedenMaarten <r0751858@ucll.be>
2023-04-28 14:27:00 +0200

    Removed some unused statuses from CrawlStatus

--- smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/entities/CrawlStatus.java
+++ smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/entities/CrawlStatus.java
@@ -1,17 +1,8 @@
 package be.dnsbelgium.mercator.smtp.persistence.entities;
 
 public enum CrawlStatus {
-
-  // See https://dmap.sidnlabs.nl/datamodel.html
   OK,
-  MALFORMED_URL,
-  TIME_OUT,
-  UNKNOWN_HOST,
   NETWORK_ERROR,
-  CONNECTION_REFUSED,
-  PROTOCOL_ERROR,
   INVALID_HOSTNAME,
-  NO_IP_ADDRESS,
   INTERNAL_ERROR
-
 }
