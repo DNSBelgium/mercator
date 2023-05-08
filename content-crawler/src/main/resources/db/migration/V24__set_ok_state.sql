51ad66e43642963b1a2c9434cd7ffd089e5946f2
Ward Segers <ward.segers@dnsbelgium.be>
2023-05-08 17:44:41 +0200

    DD-0: add migration for ok state

--- content-crawler/src/main/resources/db/migration/V24__set_ok_state.sql
+++ content-crawler/src/main/resources/db/migration/V24__set_ok_state.sql
@@ -1,6 +1,8 @@
 UPDATE content_crawl_result
-   SET html_status='Ok'
+   SET html_status='Ok',
+       screenshot_status='Ok'
  WHERE ok=true
    AND problem is null
    AND html_status is null
+   AND screenshot_status is null
    AND html_key is not null;
