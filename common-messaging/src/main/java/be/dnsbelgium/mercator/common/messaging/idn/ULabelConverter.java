01056fbdde5df0468d006b67c8229da8ac5db950
tarsb <tars.brusselaers@gmail.com>
2023-05-26 10:35:28 +0200

    DD-34: cleaned up ULabelConverter use imported function

--- common-messaging/src/main/java/be/dnsbelgium/mercator/common/messaging/idn/ULabelConverter.java
+++ common-messaging/src/main/java/be/dnsbelgium/mercator/common/messaging/idn/ULabelConverter.java
@@ -1,22 +1,17 @@
 package be.dnsbelgium.mercator.common.messaging.idn;
 
-import com.ibm.icu.text.IDNA;
-
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
-import static com.ibm.icu.text.IDNA.NONTRANSITIONAL_TO_UNICODE;
-
 @Converter
 public class ULabelConverter implements AttributeConverter<String, String> {
-    public final static IDNA IDNA_INSTANCE = com.ibm.icu.text.IDNA.getUTS46Instance(NONTRANSITIONAL_TO_UNICODE);
 
     @Override
     public String convertToDatabaseColumn(String attribute) {
         if (attribute == null) {
             return null;
         }
-        return toUnicode(attribute);
+        return IDN2008.toUnicode(attribute);
     }
 
     @Override
@@ -25,16 +20,5 @@ public class ULabelConverter implements AttributeConverter<String, String> {
             return null;
         }
         return dbData;
-    }
-
-    private String toUnicode(String input) {
-        StringBuilder destination = new StringBuilder();
-        IDNA.Info info = new IDNA.Info();
-        var unicode = IDNA_INSTANCE.nameToUnicode(input, destination, info);
-        if (info.hasErrors()) {
-            var msg = String.format("Could not convert [%s] to UNICODE: %s", input, info.getErrors());
-            throw new IdnException(msg);
-        }
-        return unicode.toString();
     }
 }
