820549579abfd53d45b0654f2b406607ee5fb61a
VanBenedenMaarten <r0751858@ucll.be>
2023-06-05 10:49:39 +0200

    Removed unused functions

--- smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/repositories/SmtpConversationRepository.java
+++ smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/repositories/SmtpConversationRepository.java
@@ -11,12 +11,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface SmtpConversationRepository extends PagingAndSortingRepository<SmtpConversationEntity, Long> {
-  @Query(value = "select * from smtp_crawler.smtp_conversation h " +
-    "where h.ip = ?1 and  h.timestamp > ?2  " +
-    "order by h.timestamp desc " +
-    "limit 1",
-    nativeQuery = true)
-  Optional<SmtpConversationEntity> findRecentCrawlByIp(@Param("ip") String ip, @Param("date_time") ZonedDateTime dateTime);
 
   Optional<SmtpConversationEntity> findByIpAndTimestamp(String ip, ZonedDateTime timestamp);
 
