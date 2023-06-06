e9fe5c35d7219928d1ab8ddf538cf85ad3436b1d
tarsb <tars.brusselaers@gmail.com>
2023-06-06 15:18:32 +0200

    dd-512: key for child elements

--- mercator-ui/src/components/DnsRequestTableBody.jsx
+++ mercator-ui/src/components/DnsRequestTableBody.jsx
@@ -5,10 +5,10 @@ export default function DnsRequestDataTable({request, requestIndex}) {
         <>
             {request.responses.map((response, responseIndex) => (
                 <DnsResponseDataTable request={request} response={response} responseIndex={responseIndex}
-                                      key={request.id + "_" + requestIndex + "_" + responseIndex}/>
+                                      key={request.id + responseIndex}/>
             ))}
             {request.responses.length === 0 && (
-                <DnsResponseDataTable request={request} response={null} key={request.id + "." + requestIndex}/>
+                <DnsResponseDataTable request={request} response={null} key={request.id}/>
             )}
         </>
     );
