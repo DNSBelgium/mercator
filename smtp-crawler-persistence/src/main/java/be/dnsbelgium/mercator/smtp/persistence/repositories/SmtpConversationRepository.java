f32faecf11a6566c6276ae21bc4d187fe73fff17
Maarten Bosteels <maarten.bosteels@dnsbelgium.be>
2023-07-07 16:51:48 +0200

    DD-55: restore findByHostId() method (used by the UI)

--- smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/repositories/SmtpConversationRepository.java
+++ smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/repositories/SmtpConversationRepository.java
@@ -15,4 +15,10 @@ public interface SmtpConversationRepository extends PagingAndSortingRepository<S
     "where sh.visit_id = :visit_id", nativeQuery = true)
   List<SmtpConversation> findAllByVisitId(@Param("visit_id") UUID visitId);
 
+  @SuppressWarnings("unused") // method is used by the UI (via the REST API)
+  @Query(value = "select * from smtp_conversation c " +
+      "inner join smtp_host h on c.id = h.conversation " +
+      "where h.id = ?1", nativeQuery = true)
+  SmtpConversation findByHostId(@Param("host_id") Long hostId);
+
 }
