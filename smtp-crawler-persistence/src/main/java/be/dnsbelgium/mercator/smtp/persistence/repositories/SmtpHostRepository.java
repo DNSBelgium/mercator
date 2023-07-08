79a1d6be9e72d5d709cb3c6c39be0aba31375412
Maarten Bosteels <maarten.bosteels@dnsbelgium.be>
2023-07-08 12:37:58 +0200

    DD-55: add @Param to fix UI

--- smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/repositories/SmtpHostRepository.java
+++ smtp-crawler-persistence/src/main/java/be/dnsbelgium/mercator/smtp/persistence/repositories/SmtpHostRepository.java
@@ -3,6 +3,7 @@ package be.dnsbelgium.mercator.smtp.persistence.repositories;
 import be.dnsbelgium.mercator.smtp.persistence.entities.SmtpConversation;
 import be.dnsbelgium.mercator.smtp.persistence.entities.SmtpHost;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -10,6 +11,6 @@ import java.util.UUID;
 public interface SmtpHostRepository extends PagingAndSortingRepository<SmtpHost, Long> {
 
   // Do not remove. Method is used by the UI. See mercator-ui/src/components/detailsCards/SMTPCard.jsx
-  List<SmtpHost> findByVisitVisitId(UUID visitId);
+  List<SmtpHost> findByVisitVisitId(@Param("visit_id") UUID visitId);
 
 }
