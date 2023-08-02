4936905841a12c1175fca492fd940d1138e5d319
Ward Segers <ward.segers@dnsbelgium.be>
2023-08-02 15:09:15 +0200

    DD-666: muppets prometheus health check

--- muppets/src/metrics.ts
+++ muppets/src/metrics.ts
@@ -1,25 +1,56 @@
 import Prometheus from "prom-client";
 import AWS from "aws-sdk";
 
-Prometheus.collectDefaultMetrics({prefix: "muppets_"});
+Prometheus.collectDefaultMetrics({ prefix: "muppets_" });
 
-export const getContentType = () => Prometheus.register.contentType;
-export const getMetrics = () => Prometheus.register.metrics();
+async function calculateFailureRate(): Promise<number> {
+    const failed = await getValueOfMetric(getFailedToProcess());
+    const okay = await getValueOfMetric(getProcessedUrlCounter());
+    const total = failed + okay;
 
-// Metrics type
+    if (total == 0)
+        return 0.0;
+
+    return failed / total;
+}
+
+async function getValueOfMetric(metric: Prometheus.Counter): Promise<number> {
+    return (await metric.get()).values[0].value;
+}
+
+function getContentType() {
+    return Prometheus.register.contentType;
+}
+
+function getMetrics() {
+    return Prometheus.register.metrics();
+}
+
 const urlProcessedCounter = new Prometheus.Counter({
     name: "muppets_url_processed_counter",
     help: "Number of processed URL"
 });
 
-export const getProcessedUrlCounter = () => urlProcessedCounter;
+const failedToProcess = new Prometheus.Counter({
+    name: "muppets_failed_to_process",
+    help: "Failures of both snapping and processing (e.g. uploading to the storage bucket)"
+});
+function getFailedToProcess() {
+    return failedToProcess;
+}
+
+function getProcessedUrlCounter() {
+    return urlProcessedCounter;
+}
 
 const processingTimeHist = new Prometheus.Histogram({
     name: "muppets_snap_processing_time",
     help: "Processing time to scrap an URL"
 });
 
-export const getProcessingTimeHist = () => processingTimeHist;
+function getProcessingTimeHist() {
+    return processingTimeHist;
+}
 
 const urlToProcessGauge = new Prometheus.Gauge({
     name: "muppets_sqs_url_to_process_gauge",
@@ -32,37 +63,41 @@ const domainTimeOuts = new Prometheus.Counter({
 });
 
 // prometeus bucket for keeping a record of screenschot sizes
-export function getScreenshotsSizes() {
+
+const screenshotSizes = new Prometheus.Histogram({
+    name: "muppets_screenshot_sizes",
+    help: "sizes of screenshots recorded during scrape"
+});
+
+function getScreenshotsSizes() {
     return screenshotSizes;
 }
 
-const screenshotSizes =new Prometheus.Histogram({
-    name: "muppets_screenshot_sizes",
-    help: "sizes of screenshots recorded during scrape"
-})
-
 // prometeus bucket for counter where the screenshot get counted that are above a certain size
-export function getBigScreenshotCounter() {
+function getBigScreenshotCounter() {
     return bigScreenshotCounter;
 }
 
 const bigScreenshotCounter = new Prometheus.Counter({
     name: "muppets_largerScreenshot_Counter",
     help: "Amount screenshots bigger then 10 MiB"
-    }
-)
+})
 
-export const getDomainTimeOuts = () => domainTimeOuts;
+function getDomainTimeOuts() {
+    return domainTimeOuts;
+};
 
 // Helper to compute metrics
 
 function getSqsMetrics(sqs: AWS.SQS, queueUrl: string) {
-    sqs.getQueueAttributes({QueueUrl: queueUrl, AttributeNames: ["ApproximateNumberOfMessages"]})
+    sqs.getQueueAttributes({ QueueUrl: queueUrl, AttributeNames: ["ApproximateNumberOfMessages"] })
         .promise()
         .then(data => urlToProcessGauge.set(parseInt(data.Attributes!.ApproximateNumberOfMessages)))
         .catch(() => console.error("Cannot retrieve SQS metrics !"));
 }
 
-export function sqsMetrics(sqs: AWS.SQS, queueUrl: string) {
+function sqsMetrics(sqs: AWS.SQS, queueUrl: string) {
     setInterval(() => getSqsMetrics(sqs, queueUrl), 1000);
 }
+
+export { getContentType, getMetrics, getFailedToProcess as getFailureCounter, getProcessedUrlCounter, getProcessingTimeHist, getScreenshotsSizes, getBigScreenshotCounter, getDomainTimeOuts, sqsMetrics, calculateFailureRate };
