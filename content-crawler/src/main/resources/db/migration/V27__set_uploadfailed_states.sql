fa3f01c26476450748f8d6add9cadc65e93e1f2d
Ward Segers <ward.segers@dnsbelgium.be>
2023-08-23 14:59:28 +0200

    DD-637: add migration for uploadfailed that checks problems

---
+++ content-crawler/src/main/resources/db/migration/V27__set_uploadfailed_states.sql
@@ -0,0 +1,16 @@
+UPDATE content_crawl_result
+SET html_status = 'UploadFailed'
+WHERE ok = FALSE
+  AND problem ~ '.+(?:,|\[)"Upload failed for file \[(.+\/)index\.html] :.+'
+  AND html_key is null
+  AND html_status is null;
+
+UPDATE content_crawl_result
+set screenshot_status = 'UploadFailed'
+WHERE ok = FALSE
+  AND (problem ~ '.+(?:,|\[)"Upload failed for file \[(.+\/)screenshot\.(png|webp)] :.+')
+  AND screenshot_key is null
+  AND screenshot_status is null;
+
+-- Similar for har_status in the future
+-- problem ~ '(?:,|\[)"Upload failed for file \[(.+\/).+\.har] :'
