d88125f5717033fb67895fd20135f016637ee730
Ward Segers <ward.segers@dnsbelgium.be>
2023-08-23 15:32:18 +0200

    DD-637: broader problem conversion

--- content-crawler/src/main/resources/db/migration/V27__set_uploadfailed_states.sql
+++ content-crawler/src/main/resources/db/migration/V27__set_uploadfailed_states.sql
@@ -1,14 +1,14 @@
 UPDATE content_crawl_result
 SET html_status = 'UploadFailed'
 WHERE ok = FALSE
-  AND problem ~ '.+(?:,|\[)"Upload failed for file \[(.+\/)index\.html] :.+'
+  AND problem ~ '(.+,|\[)"Upload failed for file \[(.+\/)index\.html] :.*'
   AND html_key is null
   AND html_status is null;
 
 UPDATE content_crawl_result
 set screenshot_status = 'UploadFailed'
 WHERE ok = FALSE
-  AND (problem ~ '.+(?:,|\[)"Upload failed for file \[(.+\/)screenshot\.(png|webp)] :.+')
+  AND (problem ~ '(.+,|\[)"Upload failed for file \[(.+\/)screenshot\.(png|webp)] :.*')
   AND screenshot_key is null
   AND screenshot_status is null;
 
