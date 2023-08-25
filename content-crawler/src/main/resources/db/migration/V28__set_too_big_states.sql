08f759294274ca90af5015a806aa55e88db0caed
Ward Segers <ward.segers@dnsbelgium.be>
2023-08-25 13:46:03 +0200

    DD-637: add TooBig conversion to new states

--- content-crawler/src/main/resources/db/migration/V28__set_too_big_states.sql
+++ content-crawler/src/main/resources/db/migration/V28__set_too_big_states.sql
@@ -5,7 +5,7 @@ WHERE html_status IS NULL
   AND html_length IS NULL
   AND html_key IS NULL;
 
-UPDATE content_crawl_result SET screenshot_status='HtmlTooBig'
+UPDATE content_crawl_result SET screenshot_status='ScreenshotTooBig'
 WHERE screenshot_status IS NULL
   AND ok=TRUE
   AND problem IS NULL
