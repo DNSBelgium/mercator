943794f60439413c09c5cbcfe8e6818095062043
Ward Segers <ward.segers@dnsbelgium.be>
2023-08-30 15:27:16 +0200

    DD-637: add migration for several other errors

---
+++ content-crawler/src/main/resources/db/migration/V36__remaining_net_errors_screenshot_status.sql
@@ -0,0 +1,16 @@
+UPDATE content_crawl_result
+   SET screenshot_status='UnexpectedError'
+ WHERE screenshot_status IS NULL
+   AND screenshot_key IS NULL
+   AND (problem LIKE '["net::ERR_ADDRESS_INVALID at %'
+    OR  problem LIKE '["net::ERR_SOCKET_NOT_CONNECTED at %'
+    OR  problem LIKE '["net::ERR_RESPONSE_HEADERS_TRUNCATED at %'
+    OR  problem = '["Protocol error: Connection closed. Most likely the page has been closed."]'
+    OR  problem LIKE '["net::ERR_INVALID_HTTP_RESPONSE at %'
+    OR  problem LIKE '["net::ERR_CONNECTION_TIMED_OUT at %'
+    OR  problem LIKE '["net::ERR_SSL_BAD_RECORD_MAC_ALERT at %'
+    OR  problem LIKE '["net::ERR_TIMED_OUT at %'
+    OR  problem LIKE '["net::ERR_HTTP2_SERVER_REFUSED_STREAM at %'
+    OR  problem LIKE '["net::ERR_FAILED at %'
+    OR  problem LIKE '["net::ERR_UNEXPECTED_PROXY_AUTH at %'
+    OR  problem LIKE '["net::ERR_RESPONSE_HEADERS_MULTIPLE_LOCATION at %')
