90507715015ab7ada43e0b2c728c3ec9514705ad
bbram00648 <bram.baestaens@student.ucll.be>
2025-02-17 15:39:27 +0100

    cleanup unused fields

--- src/main/java/be/dnsbelgium/mercator/wappalyzer/crawler/persistence/TechnologyAnalyzerWebCrawlResult.java
+++ src/main/java/be/dnsbelgium/mercator/wappalyzer/crawler/persistence/TechnologyAnalyzerWebCrawlResult.java
@@ -4,9 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -18,17 +16,7 @@ public class TechnologyAnalyzerWebCrawlResult {
   private String visitId;
   private String domainName;
 
-  private String startUrl;
-  private String matchingUrl;
-
-  private List<String> visitedUrls;
-
   // detected technologies by the TechnologyAnalyzer
   private Set<String> detectedTechnologies;
 
-  public void abbreviateData() {
-    domainName = StringUtils.abbreviate(domainName, 255);
-    startUrl = StringUtils.abbreviate(startUrl, 255);
-    matchingUrl = StringUtils.abbreviate(matchingUrl, 255);
-  }
 }
