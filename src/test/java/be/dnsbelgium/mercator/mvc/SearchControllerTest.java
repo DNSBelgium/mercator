912bbd8fd1a97a3f4a1943ab35c54eb5142416d1
bbram00648 <bram.baestaens@student.ucll.be>
2025-02-28 08:32:07 +0100

    tests for troubleshooting

--- src/test/java/be/dnsbelgium/mercator/mvc/SearchControllerTest.java
+++ src/test/java/be/dnsbelgium/mercator/mvc/SearchControllerTest.java
@@ -1,4 +1,24 @@
+package be.dnsbelgium.mercator.mvc;
+
+import be.dnsbelgium.mercator.mvc.repository.SearchRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SearchControllerTest {
-  
+
+
+
+    @Test
+    public void tls() {
+        SearchRepository repository = new SearchRepository();
+        SearchController controller = new SearchController(repository);
+        Model model = new ConcurrentModel();
+        //controller.search(model, "dnsbelgium.be");
+        String json = controller.visit(model, "dnsbelgium.be", "tls");
+        System.out.println("json = " + json);
+    }
+
 }
--- src/test/java/be/dnsbelgium/mercator/mvc/repository/SearchRepositoryTest.java
+++ src/test/java/be/dnsbelgium/mercator/mvc/repository/SearchRepositoryTest.java
@@ -1,4 +1,21 @@
+package be.dnsbelgium.mercator.mvc.repository;
+
+import be.dnsbelgium.mercator.tls.domain.TlsCrawlResult;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SearchRepositoryTest {
-  
+
+    SearchRepository searchRepository = new SearchRepository();
+
+    @Test
+    public void findTlsCrawlResult() throws JsonProcessingException {
+        TlsCrawlResult tls = searchRepository.findTlsCrawlResult("d1894946-e41d-42f7-9096-939a18bbc3dd");
+        System.out.println("Tls: " + tls);
+
+
+    }
+
 }
