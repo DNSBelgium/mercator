06cb97974ced9f5f01efac312c7dbe2ac6843663
Maarten Bosteels <maarten.bosteels@dnsbelgium.be>
2025-03-05 12:32:35 +0100

    Code review

--- src/test/java/be/dnsbelgium/mercator/persistence/SearchControllerTest.java
+++ src/test/java/be/dnsbelgium/mercator/persistence/SearchControllerTest.java
@@ -2,16 +2,16 @@ package be.dnsbelgium.mercator.persistence;
 
 import be.dnsbelgium.mercator.mvc.SearchController;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.ui.ConcurrentModel;
 import org.springframework.ui.Model;
 
 class SearchControllerTest {
 
-
-
     @Test
     public void tls() {
+        // TODO: this  method doesn't do anything ?
         SearchRepository repository = new SearchRepository();
         SearchController controller = new SearchController(repository);
         Model model = new ConcurrentModel();
@@ -21,12 +21,17 @@ class SearchControllerTest {
     }
 
     @Test
+    @Disabled
     public void searchLatestTls() throws JsonProcessingException {
+        // TODO: use a mocked Repository when testing the SearchController
         SearchRepository repository = new SearchRepository();
+        // TODO: SearchRepository.searchlatestTlsResult should return an Optional<TlsCrawlResult>
         SearchController controller = new SearchController(repository);
         Model model = new ConcurrentModel();
         String xf = controller.searchTls(model, "dnsbelgium.be", "on");
         System.out.println(xf);
+        // TODO: do not write to System.out
+        // TODO: add relevant assert statements
 
     }
 
--- src/test/java/be/dnsbelgium/mercator/persistence/repository/SearchRepositoryTest.java
+++ src/test/java/be/dnsbelgium/mercator/persistence/repository/SearchRepositoryTest.java
@@ -4,6 +4,7 @@ import be.dnsbelgium.mercator.persistence.SearchRepository;
 import be.dnsbelgium.mercator.smtp.dto.SmtpConversation;
 import be.dnsbelgium.mercator.tls.domain.TlsCrawlResult;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -13,7 +14,9 @@ class SearchRepositoryTest {
     SearchRepository searchRepository = new SearchRepository();
 
     @Test
+    @Disabled
     public void findTlsCrawlResult() throws JsonProcessingException {
+        // TODO: this test depends on state that is not managed by the test: 'tls.parquet'
         TlsCrawlResult tls = searchRepository.findTlsCrawlResult("d1894946-e41d-42f7-9096-939a18bbc3dd");
         System.out.println("Tls: " + tls);
 
@@ -21,15 +24,23 @@ class SearchRepositoryTest {
     }
 
     @Test
+    @Disabled
     public void findSmtpConversationResult() throws JsonProcessingException {
+        // TODO: prepare data, do not depend on output of other tests
         SmtpConversation smtpConversation = searchRepository.findSmtpConversationResult("d1894946-e41d-42f7-9096-939a18bbc3dd");
         System.out.println("Smtp: " + smtpConversation);
+        // what do you want to test ? Give method a proper name
+        // TODO: add asserts
     }
 
     @Test
+    @Disabled
     public void findLatestTlsResult() throws JsonProcessingException {
+        // TODO: prepare data, do not depend on output of other tests
         Optional<String> result = searchRepository.searchlatestTlsResult();
         System.out.println(result);
+        // TODO: add asserts
+        // TODO: no System.out
     }
 
 }
