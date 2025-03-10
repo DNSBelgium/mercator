7592007407f6b3717741dbf287207f6779ef7fcc
bbram00648 <bram.baestaens@student.ucll.be>
2025-03-10 12:26:08 +0100

    added tls and web search tests

---
+++ src/test/java/be/dnsbelgium/mercator/mvc/SearchSmtpResultTest.java
@@ -0,0 +1,75 @@
+package be.dnsbelgium.mercator.mvc;
+
+import be.dnsbelgium.mercator.persistence.SmtpRepository;
+import be.dnsbelgium.mercator.smtp.dto.SmtpConversation;
+import be.dnsbelgium.mercator.test.ObjectMother;
+import be.dnsbelgium.mercator.vat.domain.WebCrawlResult;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = "be.dnsbelgium.mercator.mvc")
+public class SearchSmtpResultTest {
+
+    @MockitoBean
+    private SmtpRepository smtpRepository;
+
+    private final ObjectMother objectMother = new ObjectMother();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getSmtpIds_findsIds() throws Exception {
+        when(smtpRepository.searchVisitIds("dnsbelgium.be")).thenReturn(List.of("v101", "v102", "v103"));
+        // Might need to be modified in the future to contain more data than just Id's
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/search/smtp/ids")
+                        .param("domainName", "dnsbelgium.be"))
+                .andExpect(view().name("search-results-smtp"))
+                .andExpect(model().attributeExists( "visitIds"))
+                .andExpect(content().string(containsString("v101")))
+        ;
+
+    }
+
+    @Test
+    public void getSmtp_findsVisitDetails() throws Exception {
+        SmtpConversation smtpConversation1 = objectMother.smtpConversation1();
+        when(smtpRepository.findByVisitId("idjsfijoze-er-ze")).thenReturn(Optional.ofNullable(smtpConversation1));
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/search/smtp/id")
+                        .param("visitId", "idjsfijoze-er-ze"))
+                .andExpect(view().name("visit-details-smtp"))
+                .andExpect(model().attributeExists( "smtpConversationResults"))
+                .andExpect(content().string(containsString("software host2")));
+    }
+
+
+    @Test
+    public void getSmtpLatest_findsLatestVisitDetails() throws Exception {
+        SmtpConversation smtpConversation = objectMother.smtpConversation1();
+        when(smtpRepository.findLatestResult("dnsbelgium.be")).thenReturn(Optional.ofNullable(smtpConversation));
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/search/smtp/latest")
+                        .param("domainName", "dnsbelgium.be"))
+                .andExpect(view().name("visit-details-smtp"))
+                .andExpect(model().attributeExists( "smtpConversationResults"))
+                .andExpect(content().string(containsString("software host2")));
+    }
+
+}
---
+++ src/test/java/be/dnsbelgium/mercator/mvc/SearchTlsResultTest.java
@@ -0,0 +1,84 @@
+package be.dnsbelgium.mercator.mvc;
+
+import be.dnsbelgium.mercator.persistence.TlsRepository;
+import be.dnsbelgium.mercator.persistence.WebRepository;
+import be.dnsbelgium.mercator.test.ObjectMother;
+import be.dnsbelgium.mercator.tls.domain.TlsCrawlResult;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = "be.dnsbelgium.mercator.mvc")
+public class SearchTlsResultTest {
+
+    @MockitoBean
+    private TlsRepository tlsRepository;
+
+    private final Logger logger = getLogger(SearchTlsResultTest.class);
+
+    private final ObjectMother objectMother = new ObjectMother();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getTlsIds_findsIds() throws Exception {
+        when(tlsRepository.searchVisitIds("dnsbelgium.be")).thenReturn(List.of("v101", "v102", "v103"));
+        // Might need to be modified in the future to contain more data than just Id's
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/search/tls/ids")
+                        .param("domainName", "dnsbelgium.be"))
+                .andExpect(view().name("search-results-tls"))
+                .andExpect(model().attributeExists( "visitIds"))
+                .andExpect(content().string(containsString("v101")))
+        ;
+
+    }
+    @Test
+    public void getTls_FindsVisitDetails() throws Exception {
+        // this test assumes that the correct tlsCrawlResult object looks like the one in tlsCrawlresult1
+        TlsCrawlResult tlsCrawlResult1  = objectMother.tlsCrawlResult1();
+        when(tlsRepository.findByVisitId("aakjkjkj-ojj")).thenReturn(Optional.ofNullable(tlsCrawlResult1));
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/search/tls/id")
+                        .param("visitId", "aakjkjkj-ojj"))
+                .andExpect(view().name("visit-details-tls"))
+                .andExpect(model().attributeExists( "visitId"))
+                .andExpect(content().string(containsString("aakjkjkj-ojj")));
+
+    }
+
+    @Test
+    public void getTlsLatest_FindsLatestVisitDetails() throws Exception {
+        // this test assumes that the correct tlsCrawlResult object looks like the one in tlsCrawlresult1
+        TlsCrawlResult tlsCrawlResult1  = objectMother.tlsCrawlResult1();
+        when(tlsRepository.findLatestResult("dnsbelgium.be")).thenReturn(Optional.ofNullable(tlsCrawlResult1));
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/search/tls/latest")
+                        .param("domainName", "dnsbelgium.be"))
+                .andExpect(view().name("visit-details-tls"))
+                .andExpect(model().attributeExists( "domainName"))
+                .andExpect(content().string(containsString("aakjkjkj-ojj")));
+
+    }
+}
