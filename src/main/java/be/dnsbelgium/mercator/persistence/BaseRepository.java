e771e96392ad14e55a080a9bb3a0a88088f0b53c
Pieter Vandepitte <pieter.vandepitte@dnsbelgium.be>
2025-03-20 09:01:32 +0100

    BaseRepository to inherit other repositories from

---
+++ src/main/java/be/dnsbelgium/mercator/persistence/BaseRepository.java
@@ -0,0 +1,205 @@
+package be.dnsbelgium.mercator.persistence;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.apache.commons.text.StringSubstitutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.*;
+
+public class BaseRepository<T> {
+
+  private static final Logger logger = LoggerFactory.getLogger(BaseRepository.class);
+
+  public static class SearchVisitIdResultItem {
+    private String visitId;
+    private Instant timestamp;
+
+    public String getVisitId() {
+      return visitId;
+    }
+
+    public void setVisitId(String visitId) {
+      this.visitId = visitId;
+    }
+
+    public Instant getTimestamp() {
+      return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+      this.timestamp = timestamp;
+    }
+  }
+
+  private final ObjectMapper objectMapper;
+
+  private final String baseLocation;
+
+  private final JdbcClient jdbcClient;
+
+  private final Class<T> type;
+
+  @SneakyThrows
+  public BaseRepository(ObjectMapper objectMapper, String baseLocation, JdbcClient jdbcClient, Class<T> type) {
+    this.objectMapper = objectMapper;
+    this.baseLocation = createDestination(baseLocation);
+    this.jdbcClient = jdbcClient;
+    this.type = type;
+  }
+
+  public static boolean isURL(String dataLocation) {
+    try {
+      return new URI(dataLocation).getScheme() != null;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+  public static String createDestination(String... parts) throws IOException {
+    if (isURL(parts[0])) {
+      return String.join("/", parts);
+    } else {
+      Path destPath = Path.of(parts[0], Arrays.copyOfRange(parts, 1, parts.length));
+      Files.createDirectories(destPath);
+      return destPath.toAbsolutePath().toString();
+    }
+  }
+
+  public String getAllItemsQuery() {
+    return StringSubstitutor.replace("""
+        select * exclude (year, month) from read_parquet('${base_location}/**/*.parquet')
+        """, Map.of("base_location", baseLocation));
+  }
+
+  public String timestampField() {
+    return "crawl_timestamp";
+  }
+
+  @SneakyThrows
+  public List<SearchVisitIdResultItem> searchVisitIds(String domainName) {
+    String query = StringSubstitutor.replace("""
+        with cte_all_items as (${get_all_items_query}),
+        foo as (
+          select visit_id, ${timestamp_field} as timestamp
+          from cte_all_items 
+          where domain_name=?
+        )
+        select row_to_json(foo)
+        from foo 
+        """, Map.of("get_all_items_query", getAllItemsQuery(),
+                    "timestamp_field", timestampField()
+        ));
+
+    List<String> jsonList = jdbcClient.sql(query)
+        .param(domainName)
+        .query(String.class)
+        .list();
+    List<SearchVisitIdResultItem> found = new ArrayList<>();
+    for (String json : jsonList) {
+      SearchVisitIdResultItem result = objectMapper.readValue(json, SearchVisitIdResultItem.class);
+      found.add(result);
+    }
+    return found;
+  }
+
+  @SneakyThrows
+  public Optional<T> findByVisitId(String visitId) { /* findByDomainNameQuery , order by timestamp field desc */
+    String query = StringSubstitutor.replace("""
+        with cte_all_items as (${get_all_items_query}) 
+        select row_to_json(cte_all_items)
+        from cte_all_items 
+        where visit_id=?
+        limit 1
+        """, Map.of("get_all_items_query", getAllItemsQuery()));
+    Optional<String> json = jdbcClient.sql(query)
+        .param(visitId)
+        .query(String.class)
+        .optional();
+    if (json.isPresent()) {
+      try {
+        T result = objectMapper.readValue(json.get(), this.type);
+        logger.debug("Found: \n{}", result);
+        return Optional.of(result);
+      } catch (JsonMappingException e) {
+        logger.error("JsonMappingException {} for \n {}", e.getMessage(), json);
+        throw e;
+      }
+    }
+    return Optional.empty();
+  }
+
+  @SneakyThrows
+  public Optional<T> findLatestResult(String domainName) { /* findByDomainNameQuery, order by timestamp field desc, limit 1 */
+    String query = StringSubstitutor.replace("""
+        with cte_all_items as (${get_all_items_query}) 
+        select row_to_json(cte_all_items)
+        from cte_all_items 
+        where domain_name=?
+        order by ${timestamp_field} desc
+        limit 1
+        """, Map.of("get_all_items_query", getAllItemsQuery(),
+                    "timestamp_field", timestampField()));
+    Optional<String> json = jdbcClient.sql(query)
+        .param(domainName)
+        .query(String.class)
+        .optional();
+    if (json.isPresent()) {
+      try {
+        T result = objectMapper.readValue(json.get(), this.type);
+        logger.debug("Found: \n{}", result);
+        return Optional.of(result);
+      } catch (JsonMappingException e) {
+        logger.error("JsonMappingException {} for \n {}", e.getMessage(), json);
+        throw e;
+      }
+    }
+    return Optional.empty();
+  }
+
+  @SneakyThrows
+  public List<T> findByDomainName(String domainName) { /* getAllItemsQuery, add filter CTE to filter for visit id, limit 1 */
+    String query = StringSubstitutor.replace("""
+        with cte_all_items as (${get_all_items_query}) 
+        select row_to_json(cte_all_items)
+        from cte_all_items 
+        where domain_name=?
+        """, Map.of("get_all_items_query", getAllItemsQuery()));
+    List<String> jsonList = jdbcClient.sql(query)
+        .param(domainName)
+        .query(String.class)
+        .list();
+    List<T> found = new ArrayList<>();
+    for (String json : jsonList) {
+      T result = objectMapper.readValue(json, this.type);
+      found.add(result);
+    }
+    return found;
+  }
+
+
+  /**
+   * Stores jsonResultsLocation in the repository, i.e. exports from json to parquet (the format that the repo understands)
+   *
+   * @param jsonResultsLocation
+   */
+  public void storeResults(String jsonResultsLocation) {
+
+    jdbcClient.sql(String.format("""
+      copy (
+        select 
+          *, 
+          year(to_timestamp("%s")) as year, 
+          month(to_timestamp("%s")) as month 
+        from read_json('%s')
+      ) to '%s' (format parquet, partition_by (year, month))""", timestampField(), timestampField(), jsonResultsLocation, baseLocation)).update();
+  }
+}
---
+++ src/test/java/be/dnsbelgium/mercator/persistence/BaseRepositoryTest.java
@@ -0,0 +1,79 @@
+package be.dnsbelgium.mercator.persistence;
+
+import be.dnsbelgium.mercator.test.TestUtils;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.springframework.jdbc.core.simple.JdbcClient;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+class BaseRepositoryTest {
+
+  @TempDir
+  static Path tempDir;
+
+  @TempDir
+  static Path baseLocation;
+
+  static ObjectWriter objectWriter = TestUtils.jsonWriter();
+
+  /**
+   * Sample class to store and find
+   */
+  public static class BaseItem {
+    public Instant crawlTimestamp;
+    public String visitId;
+    public String domainName;
+
+    public BaseItem(Instant crawlTimestamp, String visitId, String domainName) {
+      this.crawlTimestamp = crawlTimestamp;
+      this.visitId = visitId;
+      this.domainName = domainName;
+    }
+  }
+
+  @Test
+  void testAll() throws IOException {
+
+    BaseRepository<BaseItem> repository = new BaseRepository<>(TestUtils.jsonReader(), baseLocation.toString(), JdbcClient.create(DuckDataSource.memory()), BaseItem.class);
+
+    // prep data
+    File jsonFile = tempDir.resolve("data.json").toFile();
+    List<BaseItem> baseItems = List.of(new BaseItem(Instant.ofEpochSecond(1), "visit-1", "domain-name-1"),
+                                       new BaseItem(Instant.ofEpochSecond(2), "visit-2", "domain-name-1"),
+                                       new BaseItem(Instant.ofEpochSecond(3), "visit-3", "domain-name-2"));
+    objectWriter.writeValue(jsonFile, baseItems);
+
+    // save data in repository
+    repository.storeResults(jsonFile.toString());
+
+    // test findByVisitId
+    Optional<BaseItem> byVisitId = repository.findByVisitId("visit-2");
+    assertTrue(byVisitId.isPresent());
+    assertEquals("visit-2", byVisitId.get().visitId);
+    assertEquals("domain-name-1", byVisitId.get().domainName);
+
+    // test findByDomainName
+    List<BaseItem> byDomainName = repository.findByDomainName("domain-name-1");
+    assertEquals(2, byDomainName.size());
+
+    // test findLatestResults
+    Optional<BaseItem> latestResult = repository.findLatestResult("domain-name-1");
+    assertTrue(latestResult.isPresent());
+    assertEquals("visit-2", latestResult.get().visitId);
+    assertEquals(Instant.ofEpochSecond(2), latestResult.get().crawlTimestamp);
+
+    // test searchVisitIds
+    List<BaseRepository.SearchVisitIdResultItem> visitIds = repository.searchVisitIds("domain-name-1");
+    assertEquals(2, visitIds.size());
+  }
+}
