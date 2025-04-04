bf99c7f00b86d2b61fef28845b96884933aaf27f
Maarten Bosteels <maarten.bosteels@dnsbelgium.be>
2025-04-04 15:15:56 +0200

    Store timestamps as timestamps in parquet

--- src/main/java/be/dnsbelgium/mercator/persistence/WebRepository.java
+++ src/main/java/be/dnsbelgium/mercator/persistence/WebRepository.java
@@ -20,15 +20,16 @@ public class WebRepository extends BaseRepository<WebCrawlResult> {
   private final String pageVisitDestination;
   private final String featuresDestination;
 
-
-
   @Override
   public String getAllItemsQuery() {
     return StringSubstitutor.replace("""
       with
-        crawl_result as (select * exclude (year, month) from read_parquet('${webCrawlDestination}/**/*.parquet', union_by_name=True)),
-        html_feature as (select * exclude (year, month) from read_parquet('${featuresDestination}/**/*.parquet', union_by_name=True)),
-        page_visit   as (select * exclude (year, month) from read_parquet('${pageVisitDestination}/**/*.parquet', union_by_name=True)),
+        crawl_result1  as (select * exclude (year, month) from read_parquet('${webCrawlDestination}/**/*.parquet', union_by_name=True)),
+        html_feature1  as (select * exclude (year, month) from read_parquet('${featuresDestination}/**/*.parquet', union_by_name=True)),
+        page_visit1    as (select * exclude (year, month) from read_parquet('${pageVisitDestination}/**/*.parquet', union_by_name=True)),
+        crawl_result   as (select * replace(epoch(crawl_started) as crawl_started, epoch(crawl_finished) as crawl_finished)  from crawl_result1),
+        html_feature   as (select * replace(epoch(crawl_timestamp) as crawl_timestamp) from html_feature1),
+        page_visit     as (select * replace(epoch(crawl_started) as crawl_started, epoch(crawl_finished) as crawl_finished) from page_visit1),
         features_per_visit as (
            select visit_id,
                   list(html_feature order by crawl_timestamp) as html_features
@@ -75,10 +76,31 @@ public class WebRepository extends BaseRepository<WebCrawlResult> {
   @Override
   public void storeResults(String jsonResultsLocation) {
     String allResultsQuery = StringSubstitutor.replace("""
-      select *,
-             year(to_timestamp(${timestampField})) as year,
-             month(to_timestamp(${timestampField})) as month
-      from read_json('${jsonFile}', field_appearance_threshold=1)
+      with
+        json_data as (
+            select *
+            from read_json('${jsonFile}', field_appearance_threshold=1)
+        ),
+        casted as (
+            select
+              visit_id                      ::varchar            as visit_id,
+              domain_name                   ::varchar            as domain_name,
+              start_url                     ::varchar            as start_url,
+              matching_url                  ::varchar            as matching_url,
+              to_timestamp(crawl_started)   ::timestamptz        as crawl_started,
+              to_timestamp(crawl_finished)  ::timestamptz        as crawl_finished,
+              vat_values                    ::varchar[]          as vat_values,
+              visited_urls                  ::varchar[]          as visited_urls,
+              page_visits                                        as page_visits,
+              html_features                                      as html_features,
+              detected_technologies         ::varchar[]          as detected_technologies,
+            from json_data
+         )
+      select
+            *,
+            year(crawl_started)  as year,
+            month(crawl_started) as month
+      from casted
       """, Map.of(
         "timestampField", this.timestampField(),
         "jsonFile", jsonResultsLocation
@@ -93,7 +115,8 @@ public class WebRepository extends BaseRepository<WebCrawlResult> {
                 select * exclude (html_features, page_visits)
                 from all_results
               )
-            to '${webCrawlDestination}' (FORMAT parquet, PARTITION_BY (year, month), OVERWRITE_OR_IGNORE, FILENAME_PATTERN 'web_{uuid}' )
+            to '${webCrawlDestination}'
+            (FORMAT parquet, PARTITION_BY (year, month), OVERWRITE_OR_IGNORE, FILENAME_PATTERN 'web_{uuid}' )
             """, Map.of(
               "allResultsQuery", allResultsQuery,
               "webCrawlDestination", this.webCrawlDestination
@@ -105,16 +128,16 @@ public class WebRepository extends BaseRepository<WebCrawlResult> {
     String copyPageVisits = StringSubstitutor.replace("""
        copy (
          select
-            visit_id::VARCHAR                                  as visit_id,
-            domain_name::VARCHAR                               as domain_name,
-            crawl_started::DOUBLE                              as crawl_started,
-            crawl_finished::DOUBLE                             as crawl_finished,
-            status_code::INTEGER                               as status_code,
-            url::VARCHAR                                       as url,
-            link_text::VARCHAR                                 as link_text,
-            path::VARCHAR                                      as path,
-            response_body::VARCHAR                             as response_body,
-            vat_values::VARCHAR[]                              as vat_values,
+            visit_id                      ::VARCHAR            as visit_id,
+            domain_name                   ::VARCHAR            as domain_name,
+            to_timestamp(crawl_started::double)                as crawl_started,
+            to_timestamp(crawl_finished::double)               as crawl_finished,
+            status_code                   ::INTEGER            as status_code,
+            url                           ::VARCHAR            as url,
+            link_text                     ::VARCHAR            as link_text,
+            path                          ::VARCHAR            as path,
+            response_body                 ::VARCHAR            as response_body,
+            vat_values                    ::VARCHAR[]          as vat_values,
             year,
             month
          from (
@@ -134,59 +157,59 @@ public class WebRepository extends BaseRepository<WebCrawlResult> {
     String copyHtmlFeatures = StringSubstitutor.replace("""
        copy (
           select
-              visit_id::VARCHAR                                  as visit_id,
-              crawl_timestamp::DOUBLE                            as crawl_timestamp,
-              domain_name::VARCHAR                               as domain_name,
-              url::VARCHAR                                       as url,
-              nb_imgs::INTEGER                                   as nb_imgs,
-              nb_links_int::INTEGER                              as nb_links_int,
-              nb_links_ext::INTEGER                              as nb_links_ext,
-              nb_links_tel::INTEGER                              as nb_links_tel,
-              nb_links_email::INTEGER                            as nb_links_email,
-              nb_input_txt::INTEGER                              as nb_input_txt,
-              nb_button::INTEGER                                 as nb_button,
-              nb_meta_desc::INTEGER                              as nb_meta_desc,
-              nb_meta_keyw::INTEGER                              as nb_meta_keyw,
-              nb_numerical_strings::INTEGER                      as nb_numerical_strings,
-              nb_tags::INTEGER                                   as nb_tags,
-              nb_words::INTEGER                                  as nb_words,
-              nb_letters::INTEGER                                as nb_letters,
-              html_length::INTEGER                               as html_length,
-              nb_facebook_shallow_links::INTEGER                 as nb_facebook_shallow_links,
-              nb_facebook_deep_links::INTEGER                    as nb_facebook_deep_links,
-              nb_linkedin_deep_links::INTEGER                    as nb_linkedin_deep_links,
-              nb_linkedin_shallow_links::INTEGER                 as nb_linkedin_shallow_links,
-              nb_twitter_deep_links::INTEGER                     as nb_twitter_deep_links,
-              nb_twitter_shallow_links::INTEGER                  as nb_twitter_shallow_links,
-              nb_youtube_deep_links::INTEGER                     as nb_youtube_deep_links,
-              nb_youtube_shallow_links::INTEGER                  as nb_youtube_shallow_links,
-              nb_vimeo_deep_links::INTEGER                       as nb_vimeo_deep_links,
-              nb_vimeo_shallow_links::INTEGER                    as nb_vimeo_shallow_links,
-              nb_currency_names::INTEGER                         as nb_currency_names,
-              nb_distinct_currencies::INTEGER                    as nb_distinct_currencies,
-              distance_title_final_dn::INTEGER                   as distance_title_final_dn,
-              distance_title_initial_dn::INTEGER                 as distance_title_initial_dn,
-              longest_subsequence_title_final_dn::INTEGER        as longest_subsequence_title_final_dn,
-              longest_subsequence_title_initial_dn::INTEGER      as longest_subsequence_title_initial_dn,
-              fraction_words_title_final_dn::DOUBLE              as fraction_words_title_final_dn,
-              fraction_words_title_initial_dn::DOUBLE            as fraction_words_title_initial_dn,
-              nb_distinct_words_in_title::INTEGER                as nb_distinct_words_in_title,
-              body_text_language::VARCHAR                        as body_text_language,
-              body_text_language_2::VARCHAR                      as body_text_language_2,
-              nb_distinct_hosts_in_urls::INTEGER                 as nb_distinct_hosts_in_urls,
-              external_hosts::VARCHAR[]                          as external_hosts,
-              facebook_links::VARCHAR[]                          as facebook_links,
-              twitter_links::VARCHAR[]                           as twitter_links,
-              linkedin_links::VARCHAR[]                          as linkedin_links,
-              youtube_links::VARCHAR[]                           as youtube_links,
-              vimeo_links::VARCHAR[]                             as vimeo_links,
-              title::VARCHAR                                     as title,
-              htmlstruct::VARCHAR                                as htmlstruct,
-              body_text::VARCHAR                                 as body_text,
-              meta_text::VARCHAR                                 as meta_text,
-              body_text_truncated::BOOLEAN                       as body_text_truncated,
-              meta_text_truncated::BOOLEAN                       as meta_text_truncated,
-              title_truncated::BOOLEAN                           as title_truncated,
+              visit_id                              ::VARCHAR     as visit_id,
+              to_timestamp(crawl_timestamp)                       as crawl_timestamp,
+              domain_name                           ::VARCHAR     as domain_name,
+              url                                   ::VARCHAR     as url,
+              nb_imgs                               ::INTEGER     as nb_imgs,
+              nb_links_int                          ::INTEGER     as nb_links_int,
+              nb_links_ext                          ::INTEGER     as nb_links_ext,
+              nb_links_tel                          ::INTEGER     as nb_links_tel,
+              nb_links_email                        ::INTEGER     as nb_links_email,
+              nb_input_txt                          ::INTEGER     as nb_input_txt,
+              nb_button                             ::INTEGER     as nb_button,
+              nb_meta_desc                          ::INTEGER     as nb_meta_desc,
+              nb_meta_keyw                          ::INTEGER     as nb_meta_keyw,
+              nb_numerical_strings                  ::INTEGER     as nb_numerical_strings,
+              nb_tags                               ::INTEGER     as nb_tags,
+              nb_words                              ::INTEGER     as nb_words,
+              nb_letters                            ::INTEGER     as nb_letters,
+              html_length                           ::INTEGER     as html_length,
+              nb_facebook_shallow_links             ::INTEGER     as nb_facebook_shallow_links,
+              nb_facebook_deep_links                ::INTEGER     as nb_facebook_deep_links,
+              nb_linkedin_deep_links                ::INTEGER     as nb_linkedin_deep_links,
+              nb_linkedin_shallow_links             ::INTEGER     as nb_linkedin_shallow_links,
+              nb_twitter_deep_links                 ::INTEGER     as nb_twitter_deep_links,
+              nb_twitter_shallow_links              ::INTEGER     as nb_twitter_shallow_links,
+              nb_youtube_deep_links                 ::INTEGER     as nb_youtube_deep_links,
+              nb_youtube_shallow_links              ::INTEGER     as nb_youtube_shallow_links,
+              nb_vimeo_deep_links                   ::INTEGER     as nb_vimeo_deep_links,
+              nb_vimeo_shallow_links                ::INTEGER     as nb_vimeo_shallow_links,
+              nb_currency_names                     ::INTEGER     as nb_currency_names,
+              nb_distinct_currencies                ::INTEGER     as nb_distinct_currencies,
+              distance_title_final_dn               ::INTEGER     as distance_title_final_dn,
+              distance_title_initial_dn             ::INTEGER     as distance_title_initial_dn,
+              longest_subsequence_title_final_dn    ::INTEGER     as longest_subsequence_title_final_dn,
+              longest_subsequence_title_initial_dn  ::INTEGER     as longest_subsequence_title_initial_dn,
+              fraction_words_title_final_dn         ::DOUBLE      as fraction_words_title_final_dn,
+              fraction_words_title_initial_dn       ::DOUBLE      as fraction_words_title_initial_dn,
+              nb_distinct_words_in_title            ::INTEGER     as nb_distinct_words_in_title,
+              body_text_language                    ::VARCHAR     as body_text_language,
+              body_text_language_2                  ::VARCHAR     as body_text_language_2,
+              nb_distinct_hosts_in_urls             ::INTEGER     as nb_distinct_hosts_in_urls,
+              external_hosts                        ::VARCHAR[]   as external_hosts,
+              facebook_links                        ::VARCHAR[]   as facebook_links,
+              twitter_links                         ::VARCHAR[]   as twitter_links,
+              linkedin_links                        ::VARCHAR[]   as linkedin_links,
+              youtube_links                         ::VARCHAR[]   as youtube_links,
+              vimeo_links                           ::VARCHAR[]   as vimeo_links,
+              title                                 ::VARCHAR     as title,
+              htmlstruct                            ::VARCHAR     as htmlstruct,
+              body_text                             ::VARCHAR     as body_text,
+              meta_text                             ::VARCHAR     as meta_text,
+              body_text_truncated                   ::BOOLEAN     as body_text_truncated,
+              meta_text_truncated                   ::BOOLEAN     as meta_text_truncated,
+              title_truncated                       ::BOOLEAN     as title_truncated,
               year,
               month
           from
